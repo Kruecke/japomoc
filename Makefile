48447c6e586e28667ad8eab45660d9e0914b5296
Florian Klemme <mail@florianklemme.de>
2015-02-23 16:40:41 +0100

    Tested GLFW 'Getting started'

--- Makefile
+++ Makefile
@@ -1,6 +1,6 @@
 CXX       = clang++
 CXXFLAGS += -std=c++11 -Wall \
             $(shell pkg-config --cflags glfw3)
-LDLIBS   += $(shell pkg-config --libs   glfw3)
+LDLIBS   += $(shell pkg-config --libs   glfw3) -lGL
 
 all: test
--- test.cpp
+++ test.cpp
@@ -1,27 +1,65 @@
 #include <GLFW/glfw3.h>
+#include <iostream>
+#include <memory>
+
+static void error_callback(int error, const char *description) {
+    std::cerr << description << std::endl;
+}
+
+static void key_callback(GLFWwindow *window, int key, int scancode, int action, int mods) {
+    if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS)
+        glfwSetWindowShouldClose(window, GL_TRUE);
+}
 
 int main(int argc, char **argv) {
     // Initialize GLFW
+    glfwSetErrorCallback(error_callback);
     if (!glfwInit())
         return 1;
     struct eos_glfw { ~eos_glfw() {glfwTerminate();} } eos_terminate;
 
     // Create window with OpenCL context
-    GLFWwindow *window = glfwCreateWindow(800, 600, "Test", nullptr, nullptr);
-    if (!window)
+    auto window = std::shared_ptr<GLFWwindow>(glfwCreateWindow(800, 600, "Test", nullptr, nullptr), glfwDestroyWindow);
+    if (window == nullptr)
         return 1;
 
-    // !?
-    glfwMakeContextCurrent(window);
+    // Make the window context current
+    glfwMakeContextCurrent(window.get());
+    glfwSwapInterval(1);
+
+    // Set callback function for inputs
+    glfwSetKeyCallback(window.get(), key_callback);
 
     // Loop until the user closes the window
-    while (!glfwWindowShouldClose(window)) {
-        // TODO: Render
+    while (!glfwWindowShouldClose(window.get())) {
+        // Get size of framebuffer
+        int width, height;
+        float ratio;
+        glfwGetFramebufferSize(window.get(), &width, &height);
+        ratio = width / (float) height;
         
-        // Swap buffers
-        glfwSwapBuffers(window);
+        glViewport(0, 0, width, height);
+        glClear(GL_COLOR_BUFFER_BIT);
 
-        // Poll events
+        glMatrixMode(GL_PROJECTION);
+        glLoadIdentity();
+        glOrtho(-ratio, ratio, -1.0f, 1.0f, 1.0f, -1.0f);
+        glMatrixMode(GL_MODELVIEW);
+
+        glLoadIdentity();
+        glRotatef((float) (glfwGetTime() * 50), 0.0f, 0.0f, 1.0f);
+
+        glBegin(GL_TRIANGLES);
+        glColor3f(1.0f, 0.0f, 0.0f);
+        glVertex3f(-0.6f, -0.4f, 0.0f);
+        glColor3f(0.0f, 1.0f, 0.0f);
+        glVertex3f(0.6f, -0.4f, 0.0f);
+        glColor3f(0.0f, 0.0f, 1.0f);
+        glVertex3f(0.0f, 0.6f, 0.0f);
+        glEnd();
+
+        // Swap buffers and poll events
+        glfwSwapBuffers(window.get());
         glfwPollEvents();
     }
 
