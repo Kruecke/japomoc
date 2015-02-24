f2a5300a9b781d4bc8481876c0d7e68e92df6b32
Florian Klemme <mail@florianklemme.de>
2015-02-24 23:24:41 +0100

    Keep the repo clean.

--- .ycm_extra_conf.py
+++
@@ -1,8 +0,0 @@
-# Configuration file for VIMs plugin YouCompleteMe
-#
-# Just ignore this file if you don't use that tool.
-def FlagsForFile( filename, **kwargs ):
-    return {
-        "flags": ["-std=c++11"],
-        "do_cache": True
-    }
