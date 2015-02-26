94de7504e3a18d668ac5b261f4c766a6ed3f9e4a
Florian Klemme <mail@florianklemme.de>
2015-02-26 13:42:27 +0100

    Render a very simple menu.

--- src/Menu.cpp
+++ src/Menu.cpp
@@ -2,6 +2,14 @@
 
 #include <SFML/Graphics.hpp>
 #include <cassert>
+#include <vector>
+
+Menu::Menu() : m_cursor_pos(0) {
+    m_items.emplace_back("First!");
+    m_items.emplace_back("Second");
+    m_items.emplace_back("And a third one");
+    m_items.emplace_back("One more!");
+}
 
 bool Menu::rendering_fills_scene() const {
     // Menu will cover the whole screen.
@@ -9,9 +17,8 @@ bool Menu::rendering_fills_scene() const {
 }
 
 void Menu::render_scene(sf::RenderWindow &window) const {
-    assert(m_game != nullptr);
     // TODO
-    window.draw(sf::Text("Test!", m_game->font()));
+    render_menu(window);
 }
 
 void Menu::handle_event(sf::Event &event) {
@@ -20,4 +27,23 @@ void Menu::handle_event(sf::Event &event) {
 
 void Menu::handle_other() {
     // TODO
+}
+
+void Menu::render_menu(sf::RenderWindow &window) const {
+    assert(m_game != nullptr);
+
+    const sf::Vector2f view_size = window.getView().getSize();
+
+    for (std::size_t i = 0; i < m_items.size(); ++i) {
+        sf::Text item(m_items[i], m_game->font());
+        // Center text horizontally and set vertical offset
+        sf::FloatRect bounds = item.getLocalBounds();
+        item.setPosition((view_size.x - bounds.width)  * 0.5f,
+                  (0.5f * view_size.y - bounds.height) * 0.5);
+
+        // Move to correct vertical position
+        item.move(0.0f, ((float) i / (m_items.size() - 1)) * view_size.y * 0.5f);
+
+        window.draw(item);
+    }
 }
--- src/Menu.h
+++ src/Menu.h
@@ -1,11 +1,21 @@
 #pragma once
 
+#include <string>
+#include <vector>
+
 #include "GameComponent.h"
 
 class Menu : public GameComponent {
     public:
+        Menu();
         virtual bool rendering_fills_scene() const;
         virtual void render_scene(sf::RenderWindow&) const;
         virtual void handle_event(sf::Event&);
         virtual void handle_other();
+
+    private:
+        void render_menu(sf::RenderWindow&) const;
+
+        std::vector<std::string> m_items;
+        int m_cursor_pos; //!< Menu item pointed on by the cursor.
 };
